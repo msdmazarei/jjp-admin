0a0908aa9e7fb9580a2a8984650e5ecaeef5453d
ali kashefi <47456857+alikashefi1989@users.noreply.github.com>
2019-08-24 20:27:52 +0430

    person's service is added

--- src/service/service.person.ts
+++ src/service/service.person.ts
@@ -1,11 +1,30 @@
-import { IAPI_ResponseList, BaseService } from "./service.base";
+import { IAPI_Response,IAPI_ResponseList, BaseService } from "./service.base";
 import { IPerson } from "../model/model.person";
 
 export class PersonService extends BaseService {
-    // instance = Axios.create({
-    //     baseURL: '/api',
-    //     headers: { 'authorization': 'Bearer 0be2e4cb-88b3-44a6-aeff-4eeb96e411c6' }
-    // });
+
+
+    create(book: any) {
+        return this.axiosTokenInstance.post('/books', book);
+    }
+
+    // search(limit: number, offset: number): Promise<IAPI_ResponseList<IPerson>> {
+    //     return this.axiosTokenInstance.post(`/books/_search`, { limit, offset });
+    // }
+
+    remove(bookId: string) {
+        return this.axiosTokenInstance.delete(`/books/${bookId}`);
+    }
+
+    byId(person_id: string): Promise<IAPI_Response<IPerson>> {
+        return this.axiosTokenInstance.get(`/books/${person_id}`);
+    }
+
+    update(person: any, id: string) {
+        return this.axiosTokenInstance.put(`/books/${id}`, person);
+    }
+
+
 
 
     search(limit: number, offset: number, filter?: Object): Promise<IAPI_ResponseList<IPerson>> {

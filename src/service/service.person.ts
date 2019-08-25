1e7d258e272654b6f851ab6ec3d042cee4bce359
ali kashefi <47456857+alikashefi1989@users.noreply.github.com>
2019-08-25 18:51:55 +0430

    service's added & some path is updated

--- src/service/service.person.ts
+++ src/service/service.person.ts
@@ -3,33 +3,33 @@ import { IPerson } from "../model/model.person";
 
 export class PersonService extends BaseService {
 
-
-    create(book: any) {
-        return this.axiosTokenInstance.post('/books', book);
+    
+    create(person: any) {
+        return this.axiosTokenInstance.post('/persons',person);
     }
-
-    // search(limit: number, offset: number): Promise<IAPI_ResponseList<IPerson>> {
-    //     return this.axiosTokenInstance.post(`/books/_search`, { limit, offset });
-    // }
-
-    remove(bookId: string) {
-        return this.axiosTokenInstance.delete(`/books/${bookId}`);
-    }
-
-    byId(person_id: string): Promise<IAPI_Response<IPerson>> {
-        return this.axiosTokenInstance.get(`/books/${person_id}`);
-    }
-
-    update(person: any, id: string) {
-        return this.axiosTokenInstance.put(`/books/${id}`, person);
-    }
-
-
-
-
+    
     search(limit: number, offset: number, filter?: Object): Promise<IAPI_ResponseList<IPerson>> {
         return this.axiosTokenInstance.post(`/persons/_search`, { limit, offset, filter });
         // return this.instance.post(`http://book.mazarei.id.ir/persons/_search`, { limit, offset, filter});
     }
+    // search(limit: number, offset: number): Promise<IAPI_ResponseList<IPerson>> {
+    //     return this.axiosTokenInstance.post(`/books/_search`, { limit, offset });
+    // }
+
+    remove(personId: string) {
+        return this.axiosTokenInstance.delete(`/persons/${personId}`);
+    }
+
+    byId(person_id: string): Promise<IAPI_Response<IPerson>> {
+        return this.axiosTokenInstance.get(`/persons/${person_id}`);
+    }
+
+    update(person: any, id: string) {
+        return this.axiosTokenInstance.put(`/persons/${id}`, person);
+    }
+
+
+
+
 
 }

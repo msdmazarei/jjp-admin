0bddb41bd1f035b57ad1772968032854f2ec93a5
ali kashefi <47456857+alikashefi1989@users.noreply.github.com>
2019-08-25 18:42:39 +0430

    add person icon & path - remove useless comment

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import Dashboard from "./component/dashboard/Dashboard";
 import {BookManage} from "./component/book/BookManage/BookManage";
-import User from "./component/user/User";
 import { BookSave } from "./component/book/BookSave/BookSave";
-// import { Login } from "./component/login/Login";
+import {PersonManage} from "./component/person/PersonManage/PersonManage";
+import { PersonSave } from "./component/person/PeronSave/PersonSave";
 import {Localization} from "./config/localization/localization";
+// import User from "./component/user/User";
+// import { Login } from "./component/login/Login";
 // import { any } from "prop-types";
 // import { TInternationalization } from "./config/setup";
 // import { Redirect } from "@reach/router";
@@ -14,29 +16,19 @@ var routes = [
   {
     path: "/dashboard",
     name: Localization.dashboard,
-    // rtlName: "پنل مدیریت",
     icon: "fa fa-dashboard",
     component: Dashboard,
     layout: "/admin",
     isitem: true
   },
+  // book
   {
     path: "/book/manage",
     name:Localization.book,
-    // rtlName: "کتاب ها",
     icon: "fa fa-book",
     component: BookManage,
     layout: "/admin",
     isitem: true
-  },
-  {
-    path: "/user/manage",
-    name: "User",
-    // rtlName: "کاربران",
-    icon: "fa fa-user",
-    component: User,
-    layout: "/admin",
-    isitem: false
   },
   {
     path: "/book/create",
@@ -52,59 +44,28 @@ var routes = [
     layout: "/admin",
     isitem: false
   },
-  // {
-  //   path: "/book/manage",
-  //   name:"user",
-  //   icon: "fa fa-user",
-  //   component: BookManage,
-  //   layout: "/admin",
-  //   isitem: true
-  // },
-  // {
-  //   path: "/login",
-  //   component: Login,
-  //   layout: "/admin",
-  //   isitem: false
-  // },
-  // {
-  //   path: "/notifications",
-  //   name: "dyfghjfddhghutrgfyuftd",
-  //   rtlName: "إخطارات",
-  //   icon: "tim-icons icon-bell-55",
-  //   // component: Notifications,
-  //   layout: "/admin",
-  // },
-  // {
-  //   path: "/user-profile",
-  //   name: "User Profile",
-  //   rtlName: "ملف تعريفي للمستخدم",
-  //   icon: "fa fa-user",
-  //   component: UserProfile,
-  //   layout: "/admin"
-  // },
-  // {
-  //   path: "/tables",
-  //   name: "Table List",
-  //   rtlName: "قائمة الجدول",
-  //   icon: "tim-icons icon-puzzle-10",
-  //   component: TableList,
-  //   layout: "/admin"
-  // },
-  // {
-  //   path: "/typography",
-  //   name: "Typography",
-  //   rtlName: "طباعة",
-  //   icon: "tim-icons icon-align-center",
-  //   component: Typography,
-  //   layout: "/admin"
-  // },
-  // {
-  //   path: "/rtl-support",
-  //   name: "RTL Support",
-  //   rtlName: "تغییر زبان",
-  //   icon: "tim-icons icon-world",
-  //   component: Rtl,
-  //   layout: "/rtl"
-  // }
+  // person
+  {
+    path: "/person/manage",
+    name:Localization.person,
+    icon: "fa fa-id-card",
+    component: PersonManage,
+    layout: "/admin",
+    isitem: true
+  },
+  {
+    path: "/person/create",
+    name: Localization.create_person,
+    component: PersonSave,
+    layout: "/admin",
+    isitem: false
+  },
+  {
+    path: "/person/:person_id/edit",
+    name: Localization.person_update,
+    component: PersonSave,
+    layout: "/admin",
+    isitem: false
+  },
 ];
 export default routes;

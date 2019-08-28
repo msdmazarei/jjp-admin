2ffb4dc6383b0015978355fcddcd177c0030a9fe
ali kashefi <47456857+alikashefi1989@users.noreply.github.com>
2019-08-28 11:12:03 +0430

    new service for user added

---
+++ src/service/service.user.ts
@@ -0,0 +1,35 @@
+import { IAPI_Response,IAPI_ResponseList, BaseService } from "./service.base";
+import { IPerson } from "../model/model.person";
+
+export class PersonService extends BaseService {
+
+    
+    create(user: any) {
+        return this.axiosTokenInstance.post('/users',user);
+    }
+    
+    search(limit: number, offset: number, filter?: Object): Promise<IAPI_ResponseList<IPerson>> {
+        return this.axiosTokenInstance.post(`/users/_search`, { limit, offset, filter });
+        // return this.instance.post(`http://book.mazarei.id.ir/persons/_search`, { limit, offset, filter});
+    }
+    // search(limit: number, offset: number): Promise<IAPI_ResponseList<IPerson>> {
+    //     return this.axiosTokenInstance.post(`/books/_search`, { limit, offset });
+    // }
+
+    remove(userId: string) {
+        return this.axiosTokenInstance.delete(`/users/${userId}`);
+    }
+
+    byId(user_id: string): Promise<IAPI_Response<IPerson>> {
+        return this.axiosTokenInstance.get(`/users/${user_id}`);
+    }
+
+    update(user: any, id: string) {
+        return this.axiosTokenInstance.put(`/users/${id}`, user);
+    }
+
+
+
+
+
+}

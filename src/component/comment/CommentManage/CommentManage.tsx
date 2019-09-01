f8db0fe9a85a7decdaf8cc52ecae4411d37661a5
ali kashefi <47456857+alikashefi1989@users.noreply.github.com>
2019-09-01 15:22:57 +0430

    comment manage is added

---
+++ src/component/comment/CommentManage/CommentManage.tsx
@@ -0,0 +1,623 @@
+import React from "react";
+import { Table, IProps_table } from "../../table/table";
+import { Input } from '../../form/input/Input';
+import { History } from 'history';
+import { Modal } from "react-bootstrap";
+import { IBook } from "../../../model/model.book";
+import { ToastContainer } from "react-toastify";
+import { MapDispatchToProps, connect } from "react-redux";
+import { Dispatch } from "redux";
+import { redux_state } from "../../../redux/app_state";
+import { BaseComponent } from "../../_base/BaseComponent";
+import { TInternationalization } from "../../../config/setup";
+import { IToken } from "../../../model/model.token";
+import { Localization } from "../../../config/localization/localization";
+import { BtnLoader } from "../../form/btn-loader/BtnLoader";
+import { BOOK_TYPES } from "../../../enum/Book";
+import Select from 'react-select';
+import { CommentService } from "../../../service/service.comment";
+import { IComment } from "../../../model/model.comment";
+
+/// define props & state ///////
+export interface IProps {
+  history: History;
+  internationalization: TInternationalization;
+  token: IToken;
+}
+
+interface IFilterComment {
+  book: {
+    value: string;
+    isValid: boolean;
+  };
+  title: {
+    value: string | undefined;
+    isValid: boolean;
+  };
+  tags: {
+    value: { label: string, value: string }[];
+    isValid: boolean;
+  };
+}
+interface IState {
+  comment_table: IProps_table;
+  CommentError: string | undefined;
+  pager_offset: number;
+  pager_limit: number;
+  removeModalShow: boolean;
+  prevBtnLoader: boolean;
+  nextBtnLoader: boolean;
+  filterSearchBtnLoader: boolean;
+  tableProcessLoader: boolean;
+  filter: IFilterComment,
+  setRemoveLoader: boolean;
+  tags_inputValue: string;
+}
+
+// define class of Comment 
+
+class CommentManageComponent extends BaseComponent<IProps, IState>{
+  state = {
+    comment_table: {
+      list: [],
+      colHeaders: [
+        {
+          field: "creator", title: Localization.user, cellTemplateFunc: (row: IComment) => {
+            if (row.creator) {
+              return <div title={row.creator} className="text-nowrap-ellipsis max-w-100px d-inline-block">
+                {row.creator}
+              </div>
+            }
+            return '';
+          }
+        },
+        {
+          field: "name last_name", title: Localization.full_name, cellTemplateFunc: (row: IComment) => {
+            if (row.person) {
+              return <div title={this.getPersonFullName(row.person)} className="text-nowrap-ellipsis max-w-150px d-inline-block">
+                {this.getPersonFullName(row.person)}
+              </div>
+            }
+            return '';
+          }
+        },
+        {
+          field: "body", title: Localization.comment, cellTemplateFunc: (row: IComment) => {
+            if (row.person) {
+              return <div title={row.body} className="text-nowrap-ellipsis max-w-200px d-inline-block">
+                {row.body}
+              </div>
+            }
+            return '';
+          }
+        },
+        {
+          field: "book title", title: Localization.title, cellTemplateFunc: (row: IComment) => {
+            if (row.book && row.book.title) {
+              return <div title={row.book.title} className="text-nowrap-ellipsis max-w-150px d-inline-block">
+                {row.book.title}
+              </div>
+            }
+            return '';
+          }
+        },
+        {
+          field: "likes", title: Localization.like,cellTemplateFunc: (row: IComment) => {
+            if (row.likes) {
+              return <div title={row.likes.toLocaleString()} className="text-nowrap-ellipsis max-w-150px d-inline-block">
+                {row.likes}
+              </div>
+            }
+            else {
+              return <div className="text-muted text-center">-</div>;
+            }
+          }
+        },
+        {
+          field: "reports", title: Localization.reports,cellTemplateFunc: (row: IComment) => {
+            if (row.reports) {
+              return <div title={row.reports.toLocaleString()} className="text-nowrap-ellipsis max-w-150px d-inline-block">
+                {row.reports}
+              </div>
+            }
+            else {
+              return <div className="text-muted text-center">-</div>;
+            }
+          }
+        },
+        {
+          field: "book images", title: Localization.images, templateFunc: () => {
+            return <b>{Localization.images}</b>
+          },
+          cellTemplateFunc: (row: IComment) => {
+            if (row.book && row.book.images) {
+              return <div className="text-center" >
+                <div className="d-inline-block w-100px h-100px">
+                  <img className="max-w-100px max-h-100px" src={"/api/serve-files/" + row.book.images[0]} alt="" onError={e => this.bookImageOnError(e)} />
+                </div>
+              </div>
+            }
+            else {
+              return <div className="text-center">
+                <div className="d-inline-block w-100px h-100px">
+                  <img className="max-w-100px max-h-100px" src={this.defaultBookImagePath} alt="" />
+                </div>
+              </div>
+            }
+          }
+        },
+        {
+          field: "book type", title: Localization.type,cellTemplateFunc: (row: IComment) => {
+            if (row.book && row.book.type) {
+              const b_type: any = row.book.type;
+              const b_t: BOOK_TYPES = b_type;
+              return Localization.book_type_list[b_t];
+            }
+            else {
+              return <div className="text-muted text-center">-</div>;
+            }
+          }
+        },
+      ],
+      actions: [
+        { text: <i title={Localization.remove} className="table-action-shadow-hover fa fa-trash text-danger pt-2 mt-1"></i>, ac_func: (row: any) => { this.onShowRemoveModal(row) } },
+      ]
+    },
+    CommentError: undefined,
+    pager_offset: 0,
+    pager_limit: 5,
+    prevBtnLoader: false,
+    nextBtnLoader: false,
+    filterSearchBtnLoader: false,
+    tableProcessLoader: false,
+    removeModalShow: false,
+    filter: {
+      book:{
+        value: "true",
+        isValid: true,
+      },
+      title: {
+        value: undefined,
+        isValid: true,
+      },
+      tags: {
+        value: [],
+        isValid: true
+      }
+    },
+    setRemoveLoader: false,
+    tags_inputValue: '',
+  }
+
+  selectedComment: IComment | undefined;
+  private _commentService = new CommentService();
+
+  constructor(props: IProps) {
+    super(props);
+    this._commentService.setToken(this.props.token)
+  }
+
+  // delete modal function define
+
+  onShowRemoveModal(comment: IComment) {
+    this.selectedComment = comment;
+    this.setState({ ...this.state, removeModalShow: true });
+  }
+
+  onHideRemoveModal() {
+    this.selectedComment = undefined;
+    this.setState({ ...this.state, removeModalShow: false });
+  }
+
+  async onRemoveComment(comment_id: string) {
+    this.setState({ ...this.state, setRemoveLoader: true });
+    let res = await this._commentService.remove(comment_id).catch(error => {
+      this.handleError({ error: error.response });
+      this.setState({ ...this.state, setRemoveLoader: false });
+    });
+    if (res) {
+      this.setState({ ...this.state, setRemoveLoader: false });
+      this.apiSuccessNotify();
+      this.fetchComments();
+      this.onHideRemoveModal();
+    }
+  }
+
+  render_delete_modal(selectedComment: any) {
+    if (!this.selectedComment || !this.selectedComment.id) return;
+    return (
+      <>
+        <Modal show={this.state.removeModalShow} onHide={() => this.onHideRemoveModal()}>
+          <Modal.Body>
+            <p style={{ maxWidth: '200px', whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }} >
+              <span className="text-muted">
+                {Localization.comment}:&nbsp;
+            </span>
+              {this.selectedComment.body}
+            </p>
+            <p className="text-danger">{Localization.msg.ui.item_will_be_removed_continue}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <button className="btn btn-light shadow-default shadow-hover" onClick={() => this.onHideRemoveModal()}>{Localization.close}</button>
+            <BtnLoader
+              btnClassName="btn btn-danger shadow-default shadow-hover"
+              onClick={() => this.onRemoveComment(selectedComment.id)}
+              loading={this.state.setRemoveLoader}
+            >
+              {Localization.remove}
+            </BtnLoader>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+
+  // define axios for give data
+
+  componentDidMount() {
+    this.fetchComments();
+  }
+
+  async fetchComments() {
+    let res = await this._commentService.search(
+      this.state.pager_limit,
+      this.state.pager_offset,
+      this.getFilter()
+    ).catch(error => {
+      this.handleError({ error: error.response });
+      this.setState({
+        ...this.state,
+        prevBtnLoader: false,
+        nextBtnLoader: false,
+        tableProcessLoader: false,
+        filterSearchBtnLoader: false,
+      });
+    });
+    if (res) {
+      this.setState({
+        ...this.state, comment_table: {
+          ...this.state.comment_table,
+          list: res.data.result
+        },
+        prevBtnLoader: false,
+        nextBtnLoader: false,
+        tableProcessLoader: false,
+        filterSearchBtnLoader: false,
+      });
+    }
+  }
+
+  // previous button create
+
+  pager_previous_btn_render() {
+    if (this.state.comment_table.list && (this.state.comment_table.list! || []).length) {
+      return (
+        <>
+          {
+            this.state.pager_offset > 0 &&
+            <BtnLoader
+              disabled={this.state.tableProcessLoader}
+              loading={this.state.prevBtnLoader}
+              btnClassName="btn btn-outline-info pull-left shadow-default shadow-hover"
+              onClick={() => this.onPreviousClick()}
+            >
+              {Localization.previous}
+            </BtnLoader>
+          }
+        </>
+      );
+    } else if (this.state.comment_table.list && !(this.state.comment_table.list! || []).length) {
+      return (
+        <>
+          {
+            this.state.pager_offset > 0 &&
+            <BtnLoader
+              disabled={this.state.tableProcessLoader}
+              loading={this.state.prevBtnLoader}
+              btnClassName="btn btn-outline-info pull-left shadow-default shadow-hover"
+              onClick={() => this.onPreviousClick()}
+            >
+              {Localization.previous}
+            </BtnLoader>
+          }
+        </>
+      );
+
+    } else if (this.state.CommentError) {
+      return;
+    } else {
+      return;
+    }
+  }
+
+  // next button create
+
+  pager_next_btn_render() {
+    if (this.state.comment_table.list && (this.state.comment_table.list! || []).length) {
+      return (
+        <>
+          {
+            !(this.state.pager_limit > (this.state.comment_table.list! || []).length) &&
+            <BtnLoader
+              disabled={this.state.tableProcessLoader}
+              loading={this.state.nextBtnLoader}
+              btnClassName="btn btn-outline-info pull-right shadow-default shadow-hover"
+              onClick={() => this.onNextClick()}
+            >
+              {Localization.next}
+            </BtnLoader>
+          }
+        </>
+      );
+    } else if (this.state.comment_table.list && !(this.state.comment_table.list! || []).length) {
+      return;
+    } else if (this.state.comment_table.list) {
+      return;
+    } else {
+      return;
+    }
+  }
+
+
+  // on previous click
+
+  onPreviousClick() {
+    this.setState({
+      ...this.state,
+      pager_offset: this.state.pager_offset - this.state.pager_limit,
+      prevBtnLoader: true,
+      tableProcessLoader: true,
+
+    }, () => {
+      this.gotoTop();
+      this.fetchComments()
+    });
+  }
+
+  // on next click
+
+  onNextClick() {
+    this.setState({
+      ...this.state,
+      pager_offset: this.state.pager_offset + this.state.pager_limit,
+      nextBtnLoader: true,
+      tableProcessLoader: true,
+    }, () => {
+      this.gotoTop();
+      this.fetchComments()
+    });
+  }
+
+  //////   tag filter //////////////
+
+  handle_tagsKeyDown(event: any/* SyntheticKeyboardEvent<HTMLElement> */) {
+    if (!this.state.tags_inputValue) return;
+    switch (event.key) {
+      case 'Enter':
+      case 'Tab':
+        const newVal = this.state.tags_inputValue;
+        this.setState({
+          ...this.state,
+          filter: {
+            ...this.state.filter,
+            tags: {
+              ...this.state.filter.tags,
+              value: [
+                ...this.state.filter.tags.value,
+                { label: newVal, value: newVal }
+              ]
+            }
+          },
+          tags_inputValue: ''
+        });
+        event.preventDefault();
+    }
+  };
+
+
+  handleSelectInputChange(value: any[], inputType: any) {
+    // let isValid;
+    // if (!value || !value.length) {
+    //   isValid = false;
+    // } else {
+    //   isValid = true;
+    // }
+    this.setState({
+      ...this.state,
+      filter: {
+        ...this.state.filter,
+        [inputType]: { value: value || [], isValid: true }
+      }
+    })
+
+  }
+
+
+
+
+
+  /////  onChange & search & reset function for search box ///////////
+
+  handleFilterInputChange(value: string, isValid: boolean) {
+    this.setState({
+      ...this.state,
+      filter: {
+        ...this.state.filter,
+        title: {
+          value, isValid
+        }
+      },
+    });
+  }
+
+  filterReset() {
+    this.setState({
+      ...this.state, filter: {
+        ...this.state.filter,
+        title: {
+          value: undefined,
+          isValid: true
+        },
+      },
+      prevBtnLoader: false,
+      nextBtnLoader: false,
+    });
+  }
+
+  filterSearch() {
+    this.setState({
+      ...this.state,
+      filterSearchBtnLoader: true,
+      tableProcessLoader: true,
+      pager_offset: 0
+    }, () => {
+      // this.gotoTop();
+      this.setFilter();
+      this.fetchComments()
+    });
+  }
+
+  private _filter: IFilterComment = {
+    book: { value: "true", isValid: true },
+    title: { value: undefined, isValid: true },
+    tags: { value: [], isValid: true },
+  };
+  isFilterEmpty(): boolean {
+    if (this._filter.book.value) {
+      return false;
+    }
+    if (this._filter.title.value) {
+      return false;
+    }
+    if (this._filter.tags.value.length) {
+      return false;
+    }
+    return true;
+  }
+  setFilter() {
+    this._filter = { ...this.state.filter };
+  }
+  getFilter() {
+    if (!this.isFilterEmpty()) {
+      let obj: any = {};
+      if (this._filter.title.isValid) {
+        obj['title'] = this._filter.title.value;
+      }
+      if (this._filter.title.isValid) {
+        obj['book'] = "true";
+      }
+      if (this._filter.tags.isValid) {
+        if (this._filter.tags.value.length) {
+          obj['tags'] = this._filter.tags.value.map(t => t.value);
+        }
+        // obj['tags'] = this._filter.tags.value.length?;
+      }
+      return obj;
+    }
+    return;
+  }
+
+  //////render call Table component //////
+
+  render() {
+    return (
+      <>
+        <div className="content">
+          <div className="row">
+            <div className="col-12">
+              <h2 className="text-bold text-dark pl-3">{Localization.comment}</h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <div className="template-box mb-4">
+                <div className="row">
+                  <div className="col-4">
+                    <Input
+                      onChange={(value: string, isValid) => this.handleFilterInputChange(value, isValid)}
+                      label={Localization.comment}
+                      placeholder={Localization.comment}
+                      defaultValue={this.state.filter.title.value}
+                    />
+                  </div>
+                  <div className="col-8 d-none">
+                    <div className="form-group">
+                      <label htmlFor="">{Localization.tags}</label>
+                      <Select
+                        isMulti
+                        onChange={(value: any) => this.handleSelectInputChange(value, "tags")}
+                        value={this.state.filter.tags.value}
+                        placeholder={Localization.tags}
+                        onKeyDown={(e) => this.handle_tagsKeyDown(e)}
+                        inputValue={this.state.tags_inputValue}
+                        menuIsOpen={false}
+                        components={{
+                          DropdownIndicator: null,
+                        }}
+                        isClearable
+                        onInputChange={(inputVal) => this.setState({ ...this.state, tags_inputValue: inputVal })}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-12">
+                    <BtnLoader
+                      disabled={this.state.tableProcessLoader}
+                      loading={this.state.filterSearchBtnLoader}
+                      btnClassName="btn btn-info shadow-default shadow-hover pull-right ml-3"
+                      onClick={() => this.filterSearch()}
+                    >
+                      {Localization.search}
+                    </BtnLoader>
+                    <BtnLoader
+                      // disabled={this.state.tableProcessLoader}
+                      loading={false}
+                      btnClassName="btn btn-warning shadow-default shadow-hover pull-right"
+                      onClick={() => this.filterReset()}
+                    >
+                      {Localization.reset}
+                    </BtnLoader>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <Table list={this.state.comment_table.list} colHeaders={this.state.comment_table.colHeaders} actions={this.state.comment_table.actions}></Table>
+              <div>
+                {this.pager_previous_btn_render()}
+                {this.pager_next_btn_render()}
+              </div>
+            </div>
+          </div>
+        </div>
+        {this.render_delete_modal(this.selectedComment)}
+        <ToastContainer {...this.getNotifyContainerConfig()} />
+      </>
+    );
+  }
+}
+
+const dispatch2props: MapDispatchToProps<{}, {}> = (dispatch: Dispatch) => {
+  return {
+  };
+};
+
+const state2props = (state: redux_state) => {
+  return {
+    internationalization: state.internationalization,
+    token: state.token,
+  };
+};
+
+export const CommentManage = connect(
+  state2props,
+  dispatch2props
+)(CommentManageComponent);

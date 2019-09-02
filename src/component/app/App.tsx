f1224166f3c8f2efffaa731277af2f8783dac37a
alikashefi1989 <kashefi.eng@gmail.com>
2019-09-02 17:45:34 +0430

    order manage page path added to route

--- src/component/app/App.tsx
+++ src/component/app/App.tsx
@@ -43,6 +43,9 @@ const appRoutes = (
             <Route exact path="/user/create" render={props => <AdminLayout {...props} />} />
             <Route exact path="/user/:user_id/edit" render={props => <AdminLayout {...props} />} />
             <Route exact path="/comment/manage" render={props => <AdminLayout {...props} />} />
+            <Route exact path="/order/manage" render={props => <AdminLayout {...props} />} />
+            {/* <Route exact path="/order/create" render={props => <AdminLayout {...props} />} /> */}               // not added
+            {/* <Route exact path="/order/:order_id/edit" render={props => <AdminLayout {...props} />} /> */}       // not added
             {/* <Route exact path="/user/manage" render={props => <AdminLayout {...props} />} /> */}
             {/* <Route exact path="/" component={() => <Redirect to="/dashboard" />} /> */}
             {/* <RouteLayoutMain exact path="/dashboard" component={Dashboard} /> */}

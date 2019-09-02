a30064386abc8c44586a2551d8889da64586788d
alikashefi1989 <kashefi.eng@gmail.com>
2019-09-02 17:46:18 +0430

    order service is modified

--- src/service/service.order.ts
+++ src/service/service.order.ts
@@ -1,4 +1,4 @@
-import { BaseService, IAPI_Response } from './service.base';
+import { BaseService,IAPI_Response , IAPI_ResponseList } from './service.base';
 
 interface IOrderItem {
     book_id: string;
@@ -8,6 +8,14 @@ interface IOrderItem {
 export type IOrderItems = IOrderItem[];
 
 export class OrderService extends BaseService {
+
+    search(limit: number, offset: number, filter?: Object): Promise<IAPI_ResponseList<any>> {
+        return this.axiosTokenInstance.post(`/orders/_search`, { limit, offset, filter });
+    }
+
+    remove(bookId: string) {
+        return this.axiosTokenInstance.delete(`/books/${bookId}`);
+    }
 
     order(items: IOrderItems, person_id: string): Promise<IAPI_Response<any>> {
         return this.axiosTokenInstance.post('/orders', { items, person_id });

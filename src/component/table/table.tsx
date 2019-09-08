98feea2ebf592d40352578bb6d9db76ac01c8655
alikashefi1989 <kashefi.eng@gmail.com>
2019-09-08 13:26:38 +0430

    add dropdown to action column

--- src/component/table/table.tsx
+++ src/component/table/table.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { Localization } from '../../config/localization/localization';
+import Dropdown from "react-bootstrap/Dropdown";
 // define props of table
 
 export interface IProps_table {
@@ -29,14 +30,15 @@ export class Table<T extends IProps_table> extends React.Component<T>{
     render() {
         return (
             <>
-                <div className="table-responsive table-responsive-sm table-responsive-md table-responsive-lg table-responsive-xl template-box ps mb-3">
+                <div className="table-responsive table-responsive-sm-table responsive-md-table responsive-lg-table responsive-xl template-box mb-3">
                     <table className="table table-striped table-bordered table-hover table-sm table-bordered bg-white mb-0">
                         <thead className="thead-light">
                             <tr className="table-light">
                                 {(this.props.colHeaders).map((h, index) => (
                                     <Fragment key={index}>
                                         {
-                                            h.templateFunc ?
+                                            h.templateFunc
+                                                ?
                                                 <th scope="font-weight-bold" >{h.templateFunc()}</th>
                                                 :
                                                 <th scope="font-weight-bold" >{h.title}</th>
@@ -50,9 +52,9 @@ export class Table<T extends IProps_table> extends React.Component<T>{
                             </tr>
                         </thead>
                         <tbody>
-
                             {
-                                (this.props.list && this.props.list.length) ?
+                                (this.props.list && this.props.list.length)
+                                    ?
                                     this.props.list.map((row, index) => (
                                         <tr className="table-light" key={index}>
                                             {this.props.colHeaders.map((ch, index) => (
@@ -66,21 +68,42 @@ export class Table<T extends IProps_table> extends React.Component<T>{
                                                     }
                                                 </td>
                                             ))}
-                                            {this.props.actions ?
-                                                <td className="py-0 px-1 text-center">
+                                            {this.props.actions
+                                                ?
+                                                <td className="py-0 px-1 text-center action-col">
                                                     {
-                                                        this.props.actions ?
-                                                            this.props.actions.map((ac, index) => (
-                                                                (this.getAccess_action(row, ac.access))
-                                                                    ?
-                                                                    <div className="text-center" key={index} onClick={() => ac.ac_func(row)}>{ac.text}</div>
-                                                                    :
-                                                                    ''
-                                                            ))
-                                                            : ''
+                                                        this.props.actions
+                                                            ?
+                                                            <Dropdown>
+                                                                <Dropdown.Toggle
+                                                                    title={Localization.more}
+                                                                    split
+                                                                    variant="light"
+                                                                    className="px-3 bg-light btn"
+                                                                    id="dropdown-split-basic"
+                                                                >
+                                                                    <i title={Localization.more} className="fa fa-ellipsis-v"></i>
+                                                                </Dropdown.Toggle>
+                                                                <Dropdown.Menu
+                                                                className="dropdown-menu-right"
+                                                                >
+                                                                    {
+                                                                        this.props.actions.map((ac, index) => (
+                                                                            (this.getAccess_action(row, ac.access))
+                                                                                ?
+                                                                                <Dropdown.Item className="text-center" key={index} onClick={() => ac.ac_func(row)}>{ac.text}</Dropdown.Item>
+                                                                                :
+                                                                                ''
+                                                                        ))
+                                                                    }
+                                                                </Dropdown.Menu>
+                                                            </Dropdown >
+                                                            :
+                                                            ''
                                                     }
                                                 </td>
-                                                : ''
+                                                :
+                                                ''
                                             }
                                         </tr>
                                     ))

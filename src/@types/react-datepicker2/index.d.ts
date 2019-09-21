ee4dd8b238f77f7a0c05b2595d1d5a55ab0a51c0
ali kashefi <kashefi.eng@gmail.com>
2019-09-21 20:18:52 +0430

    types define for datepicker added

---
+++ src/@types/react-datepicker2/index.d.ts
@@ -0,0 +1,20 @@
+declare module 'react-datepicker2' {
+  // const value: string;
+  // export = value;
+
+  interface IProps {
+    onchange?: (timeStamp: number) => void;
+    isGregorian?: boolean;
+    timePicker?: boolean;
+    disabled?: boolean;
+    min?: number;
+    max?: number;
+    ranges?: {}[];
+    inputFormat?: string;
+    inputJalaaliFormat?: string;
+    value: any;
+  }
+
+  export default class DatePicker<any> extends React.Component<any>{ }
+
+}

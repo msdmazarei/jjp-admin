5ebedeac2f4c6e95ba99c9cce3454cc8895a0395
ali kashefi <kashefi.eng@gmail.com>
2019-09-22 14:20:43 +0330

    datepicker is modified

--- src/component/form/app-datePicker/AppDatePicker.tsx
+++ src/component/form/app-datePicker/AppDatePicker.tsx
@@ -1,112 +1,81 @@
 import React from 'react';
 import { connect, MapDispatchToProps } from 'react-redux';
 import { Dispatch } from 'redux';
-import DatePicker from 'react-datepicker2';
 import { TInternationalization } from '../../../config/setup';
 import { IToken } from '../../../model/model.token';
 import { BaseComponent } from '../../_base/BaseComponent';
 import { redux_state } from '../../../redux/app_state';
-import moment from 'moment';
-import moment_jalaali from "moment-jalaali";
-// import 'moment/locale/fa';
-// import moment from 'moment';
-// moment_jalaali.locale('fa');
-// moment.locale('fa');
+// import { DateTimeInput, DateTimeInputSimple, DateInput, DateInputSimple } from 'react-hichestan-datetimepicker';
+import { DateInput } from 'react-hichestan-datetimepicker';
 interface IState {
-    value: any;
-    timeStamp: number;
-    isGregorian: boolean;
+    value: string;
+    timeStamp:number;
 }
 
 interface IProps {
+    history?: History;
     internationalization: TInternationalization;
     token: IToken;
-    onchange?: (timeStamp: number) => void;
-    isGregorian?: boolean;
-    timePicker?: boolean;
-    disabled?: boolean;
-    min?: number;
-    max?: number;
-    ranges?: {}[];
-    inputFormat?: string;
-    inputJalaaliFormat?: string;
-    class?: string;
-    value?: any; // number;
+    defaultValue:string;
+    value?:string;
+    name?:string;
+    className?:string;
+    placeholder?:string;
+    style?:{};
+    gregorian?:boolean;
+    autoOk?:boolean;
+    onChange?:(timeStamp : number) => void
 }
 
 class AppDatePickerComponent extends BaseComponent<IProps, IState> {
     state = {
-        timeStamp: 0,
-        // value: moment_jalaali('1396/7/6', 'jYYYY/jM/jD'), // moment_jalaali(),
-        // value: undefined,
-        // value: moment(),
-        // value: moment('1396/7/6', 'jYYYY/jM/jD')
-        isGregorian: false,
-        // value: this.props.value ? moment_jalaali.unix(this.props.value).format("YYYY/MM/DD") : undefined
-        // value: this.props.value ? moment_jalaali.unix(this.props.value) : undefined
-        // value: this.props.value ? moment_jalaali(this.props.value) : undefined,
-        // value: this.props.value ? moment.unix(this.props.value) : undefined
-        value: moment_jalaali('1991-10-22')
+        value: '',
+        timeStamp:0,
     }
 
-    constructor(p: IProps) {
-        super(p);
-        // let egfbd = moment();
-        // let csacegfbd = moment_jalaali();
-        // debugger
-        // console.log(moment_jalaali('1396/7/6', 'jYYYY/jM/jD'));
-        // console.log(moment_jalaali.unix(this.props.value));
-    }
-
-    setDate(value: any) {
-        debugger;
-        if (value && this.state.value && value.unix() === this.state.value.unix()) return;
+    componentDidMount(){
         this.setState({
             ...this.state,
-            // timeStamp: new Date(value).getTime() / 1000,
-            value: value
-        }, () => {
-            if (this.props.onchange) {
-                this.props.onchange(this.state.timeStamp)
-                // console.log(value);
-                // console.log(this.state.timeStamp)
-            }
-        });
+            value: this.props.defaultValue
+        })
+    }
 
+    handleChange(value: any) {
+        const t = value.target;
+        const val = t.value;
+        const ts = new Date(val).getTime()/1000;
+        this.setState({
+            ...this.state,
+            value: val,
+            timeStamp:ts,
+        },() => this.transferTimestamp());
     }
-    chage() {
-        this.setState({ isGregorian: !this.state.isGregorian })
+
+    transferTimestamp(){
+        if(this.props.onChange){
+            this.props.onChange(this.state.timeStamp)
+        }
     }
+
+    
     render() {
         return (
-            <>
-                <div dir={''}>
-                    <div className="btn btn-danger" onClick={() => this.chage()}>click</div>
-
-                    <DatePicker
-                        value={this.state.value}
-                        // isGregorian={this.props.isGregorian}
-                        // isGregorian={false}
-                        // disabled={this.props.disabled}
-                        // timePicker={this.props.timePicker}
-                        // min={this.props.min}
-                        // max={this.props.max}
-                        // isGregorian={this.state.isGregorian}
-                        // ranges={this.props.ranges}
-                        // inputFormat={this.props.inputFormat}
-                        // inputJalaaliFormat={this.props.inputJalaaliFormat}
-                        onChange={(value: any) => this.setDate(value)}
-                        // className={this.props.class}
-
-                        // initialDate: '1991-10-22',
-                        displayFormat={'jYYYY/jM/jD'}
-                        timePicker={false}
-                        isGregorian={false}
-                    ></DatePicker>
-                </div>
-            </>
+            <div>
+                <DateInput
+                    defaultValue={this.props.defaultValue}
+                    value={this.state.value}
+                    name={this.props.name}
+                    autoOk={this.props.autoOk}
+                    className={this.props.className}
+                    onChange={(value: any) => this.handleChange(value)}
+                    placeholder={this.props.placeholder}
+                    style={this.props.style}
+                    gregorian={this.props.gregorian}
+                />
+            </div>
         )
     }
+
 }
 
 const dispatch2props: MapDispatchToProps<{}, {}> = (dispatch: Dispatch) => {

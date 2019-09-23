d05e2174be0c2e74b34475eb8b3d8116b6f74475
ali kashefi <kashefi.eng@gmail.com>
2019-09-23 19:45:42 +0330

    add chart component to custom admin report list

--- src/component/tool/reports/ReportUtils.ts
+++ src/component/tool/reports/ReportUtils.ts
@@ -1,9 +1,13 @@
 import { ReportCommentTable } from './report-table-1/ReportTable1';
+import {ReportBestSellsChart} from './report-chart-1/ReportChart1'
 
-export type TReport = 'newst_comment';
+export type TReport = 'newst_comment' | 'best_sells_chart'
+
 
 export const reportListMapCmp: {
     [key in TReport]: any;
 } = {
-    newst_comment: ReportCommentTable
+    newst_comment: ReportCommentTable,
+    best_sells_chart: ReportBestSellsChart
 }
+

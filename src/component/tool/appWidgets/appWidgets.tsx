bced6a463d75b4084fb7dd85620d9ce6dda74477
ali kashefi <kashefi.eng@gmail.com>
2019-09-25 17:53:50 +0330

    add func to give main title from child

--- src/component/tool/appWidgets/appWidgets.tsx
+++ src/component/tool/appWidgets/appWidgets.tsx
@@ -17,8 +17,8 @@ interface IState {
     minimize: boolean;
     restore: boolean;
     close: boolean;
-
-    child_tools: JSX.Element | undefined
+    child_tools: JSX.Element | undefined;
+    child_title:JSX.Element | undefined;
 }
 
 class AppWidgetsComponent extends BaseComponent<IProps, IState> {
@@ -27,6 +27,7 @@ class AppWidgetsComponent extends BaseComponent<IProps, IState> {
         restore: true,
         close: false,
         child_tools: undefined,
+        child_title:undefined,
     }
 
     minimizeFunction() {
@@ -78,8 +79,16 @@ class AppWidgetsComponent extends BaseComponent<IProps, IState> {
         }
     }
 
+    init_title(cmpTitle: JSX.Element) {
+        this.setState({ 
+            ...this.state,
+            child_title: cmpTitle });
+    }
+
     init_tools(tools: JSX.Element) {
-        this.setState({ child_tools: tools });
+        this.setState({
+            ...this.state,
+             child_tools: tools });
     }
 
     widget_header_tools() {
@@ -122,11 +131,15 @@ class AppWidgetsComponent extends BaseComponent<IProps, IState> {
                                 }>
                     <div className="widget-header px-1--mt-1 ">
                         <div className="tools external-tools">{this.state.child_tools}</div>
+                        <div className="widget-header-title">{this.state.child_title}</div>
                         <div className="tools widget-header-tools">{this.widget_header_tools()}</div>
                     </div>
                     <div className={this.state.minimize ? "d-none" : "widget-body p"}>
                         {
-                            React.cloneElement(this.props.children as any, { init_tools: (ts: JSX.Element) => this.init_tools(ts) })
+                            React.cloneElement(this.props.children as any, { 
+                                init_tools: (ts: JSX.Element) => this.init_tools(ts),
+                                init_title: (title: JSX.Element) => this.init_title(title),
+                             })
                         }
                     </div>
                     <div className={this.state.minimize ? "d-none" : "widget-footer "} >

021ba08073886fb83cf21834c23472faa39456a8
ali kashefi <kashefi.eng@gmail.com>
2019-09-25 17:53:09 +0330

    add yearly report to dashboard

--- src/component/tool/reports/ReportUtils.ts
+++ src/component/tool/reports/ReportUtils.ts
@@ -1,8 +1,9 @@
 import { ReportCommentTable } from './report-table-1/ReportTable1';
 import {ReportBestSellsChart} from './report-chart-1/ReportChart1';
+import {ReportYearSellChart} from './report-chart-2/ReportChart2';
 import {ReportlastSellWithTypeTable}  from './report-table-2/ReportTable2';
 
-export type TReport = 'newst_comment' | 'best_sells_chart' | 'last_sell_with_type'
+export type TReport = 'newst_comment' | 'best_sells_chart' | 'last_sell_with_type' | 'year_sell_with_month'
 
 
 export const reportListMapCmp: {
@@ -10,6 +11,7 @@ export const reportListMapCmp: {
 } = {
     newst_comment: ReportCommentTable,
     best_sells_chart: ReportBestSellsChart,
-    last_sell_with_type : ReportlastSellWithTypeTable
+    last_sell_with_type : ReportlastSellWithTypeTable,
+    year_sell_with_month: ReportYearSellChart
 }
 

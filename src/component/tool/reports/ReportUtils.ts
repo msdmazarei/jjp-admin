e18315b70be9207ecfa433bb9def2a97a9cd10b2
ali kashefi <kashefi.eng@gmail.com>
2019-09-28 19:05:54 +0330

    add  customer performance to dashboard

--- src/component/tool/reports/ReportUtils.ts
+++ src/component/tool/reports/ReportUtils.ts
@@ -1,10 +1,11 @@
 import { ReportCommentTable } from './report-table-1/ReportTable1';
 import {ReportBestSellsChart} from './report-chart-1/ReportChart1';
 import {ReportYearSellChart} from './report-chart-2/ReportChart2';
+import {ReportStoreCustomerPerformance} from './report-chart-4/ReportChart4';
 import {ReportPublisherSellsCompare} from './report-chart-3/ReportChart3';
 import {ReportlastSellWithTypeTable}  from './report-table-2/ReportTable2';
 
-export type TReport = 'newst_comment' | 'best_sells_chart' | 'last_sell_with_type' | 'year_sell_with_month' | 'Compear_publisher_sells'
+export type TReport = 'newst_comment' | 'best_sells_chart' | 'last_sell_with_type' | 'year_sell_with_month' | 'Compear_publisher_sells' | 'store_customer_performance'
 
 
 export const reportListMapCmp: {
@@ -14,6 +15,7 @@ export const reportListMapCmp: {
     best_sells_chart : ReportBestSellsChart,
     last_sell_with_type : ReportlastSellWithTypeTable,
     year_sell_with_month : ReportYearSellChart,
-    Compear_publisher_sells : ReportPublisherSellsCompare
+    Compear_publisher_sells : ReportPublisherSellsCompare,
+    store_customer_performance : ReportStoreCustomerPerformance,
 }
 

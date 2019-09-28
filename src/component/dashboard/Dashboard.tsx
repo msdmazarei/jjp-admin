f3efbba9b2855fdf4312b825e57a510d1aab7e04
ali kashefi <kashefi.eng@gmail.com>
2019-09-28 14:19:32 +0330

    publishers sale'chart compare add to chart

--- src/component/dashboard/Dashboard.tsx
+++ src/component/dashboard/Dashboard.tsx
@@ -38,7 +38,7 @@ class DashboardComponent extends BaseComponent<IProps, IState> {
 
   grtUserReport(uer_id: string): Promise<TReport[]> {
     return new Promise((res, rej) => {
-      res(["newst_comment","best_sells_chart","last_sell_with_type","year_sell_with_month"])
+      res(["newst_comment","best_sells_chart","last_sell_with_type","year_sell_with_month","Compear_publisher_sells"])
     })
   }
 

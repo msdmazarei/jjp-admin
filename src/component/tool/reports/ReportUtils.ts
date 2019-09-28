1a36fc95f4ea4c7da679f40ca5e242f12caa4ed1
ali kashefi <kashefi.eng@gmail.com>
2019-09-28 14:19:14 +0330

    publishers sale compare component add to reports

--- src/component/tool/reports/ReportUtils.ts
+++ src/component/tool/reports/ReportUtils.ts
@@ -1,17 +1,19 @@
 import { ReportCommentTable } from './report-table-1/ReportTable1';
 import {ReportBestSellsChart} from './report-chart-1/ReportChart1';
 import {ReportYearSellChart} from './report-chart-2/ReportChart2';
+import {ReportPublisherSellsCompare} from './report-chart-3/ReportChart3';
 import {ReportlastSellWithTypeTable}  from './report-table-2/ReportTable2';
 
-export type TReport = 'newst_comment' | 'best_sells_chart' | 'last_sell_with_type' | 'year_sell_with_month'
+export type TReport = 'newst_comment' | 'best_sells_chart' | 'last_sell_with_type' | 'year_sell_with_month' | 'Compear_publisher_sells'
 
 
 export const reportListMapCmp: {
     [key in TReport]: any;
 } = {
-    newst_comment: ReportCommentTable,
-    best_sells_chart: ReportBestSellsChart,
+    newst_comment : ReportCommentTable,
+    best_sells_chart : ReportBestSellsChart,
     last_sell_with_type : ReportlastSellWithTypeTable,
-    year_sell_with_month: ReportYearSellChart
+    year_sell_with_month : ReportYearSellChart,
+    Compear_publisher_sells : ReportPublisherSellsCompare
 }
 

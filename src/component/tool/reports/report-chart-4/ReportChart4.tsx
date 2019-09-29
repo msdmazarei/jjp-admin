01acb618c9fdd74f82e6584b0bc06ffeddd60e1f
ali kashefi <kashefi.eng@gmail.com>
2019-09-29 15:11:39 +0330

    customer performance funnel chart created

--- src/component/tool/reports/report-chart-4/ReportChart4.tsx
+++ src/component/tool/reports/report-chart-4/ReportChart4.tsx
@@ -9,7 +9,10 @@ import { IToken } from "../../../../model/model.token";
 import { BaseComponent } from "../../../_base/BaseComponent";
 import { redux_state } from "../../../../redux/app_state";
 import { Localization } from "../../../../config/localization/localization";
-import { ResponsiveContainer} from "recharts";
+import { ResponsiveContainer, Tooltip, LabelList } from "recharts";
+import { FunnelChart, Funnel } from 'recharts';
+import { PersonService } from "../../../../service/service.person";
+import { OrderService, IOrderItems } from "../../../../service/service.order";
 
 
 
@@ -22,8 +25,8 @@ export interface IProps {
 }
 
 interface IState {
-    number_of_member: number;
-    number_of_order: number;
+    number_of_member: any[];
+    number_of_order: any[];
     number_of_invoiced: number;
 }
 
@@ -32,13 +35,13 @@ class ReportStoreCustomerPerformanceComponent extends BaseComponent<IProps, ISta
     /// start of state
 
     state = {
-        number_of_member: 0,
-        number_of_order: 0,
-        number_of_invoiced: 0,
+        number_of_member:[],
+        number_of_order: [],
+        number_of_invoiced:0,
     }
     /// end of state
 
-    private _report_title: string = Localization.name_of_report.Compare_publishers_sales_by_time_period;
+    private _report_title: string = Localization.name_of_report.User_to_customer_conversion_process_chart;
 
 
     // constructor(props: IProps) {
@@ -48,9 +51,62 @@ class ReportStoreCustomerPerformanceComponent extends BaseComponent<IProps, ISta
     componentDidMount() {
         this.init_title();
         this.init_tools();
+        // this._personService.setToken(this.props.token);
+        // this._orderService.setToken(this.props.token);
+        // this.fetchPersons();
+        // this.fetchOrders();
     }
 
 
+    // start service for request order & person number in system
+        /* start test request change after
+        private _personService = new PersonService();
+        private _orderService = new OrderService();
+
+        // start member count
+        async fetchPersons() {
+            let res = await this._personService.search(1000, 0).catch(error => {
+                this.handleError({ error: error.response });
+            });
+            if (res) {
+                this.setState({
+                    ...this.state,
+                    number_of_member: res.data.result,
+                });
+            }
+        }
+        // end member count
+
+        // start number of order & invoced
+        async fetchOrders() {
+            let res = await this._orderService.search(1000,0, {}).catch(error => {
+              this.handleError({ error: error.response })});
+            if (res) {
+              this.setState({
+                ...this.state,
+                  number_of_order: res.data.result
+              },
+              () =>  this.invocied_counter(this.state.number_of_order));
+            }
+        }
+        // end number of order & invoced
+
+        async invocied_counter(list:any){
+            let invoced = 0;
+            await list.map((item:any,index:number) =>
+                item.status === "Invoiced"
+                ?
+                invoced++
+                :
+                undefined
+            )
+            this.setState({
+                ...this.state,
+                number_of_invoiced : invoced,
+            })
+        }
+        end test request change after */
+    // end service for request order & person number in system
 
 
     // start define custom tools & pass that to widget
@@ -85,7 +141,7 @@ class ReportStoreCustomerPerformanceComponent extends BaseComponent<IProps, ISta
 
     // start data set on chart from state
 
-    data_option_returner(mem: number, ord: number, inv: number) {
+    data_option_returner(mem: number, ord: number, inv: number) : any[] {
         const member: number = mem;
         const order: number = ord;
         const invoiced: number = inv;
@@ -110,21 +166,31 @@ class ReportStoreCustomerPerformanceComponent extends BaseComponent<IProps, ISta
                     'fill': 'green',
                 },
             ]
+        return data;
     }
 
     // end data set on chart from state
+
 
     // start return_report function
 
     report_status() {
 
-        const data: any = this.data_option_returner(this.state.number_of_member, this.state.number_of_order, this.state.number_of_invoiced)
-
         return <>
             <div className="col-12">
                 <div style={{ width: '100%', height: 250 }}>
                     <ResponsiveContainer>
-                       
+                        <FunnelChart>
+                            <Tooltip position={{ x: -20, y: 40 }} />
+                            <Funnel
+                                dataKey="value"
+                                // data={this.data_option_returner(this.state.number_of_member.length, this.state.number_of_order.length,this.state.number_of_invoiced)}
+                                data={this.data_option_returner(72,61,35)}
+                                isAnimationActive={true}
+                            >
+                             <LabelList dataKey="name"/>   
+                            </Funnel>
+                        </FunnelChart>
                     </ResponsiveContainer>
                 </div>
             </div>

25eb1e9ee76ec7108db8fd0a346a52a8fd2ff179
ali kashefi <kashefi.eng@gmail.com>
2019-10-07 16:40:05 +0330

    add person input and func to group  save cmp

--- src/component/group/GroupSave/GroupSave.tsx
+++ src/component/group/GroupSave/GroupSave.tsx
@@ -11,6 +11,9 @@ import { IToken } from '../../../model/model.token';
 import { ToastContainer } from 'react-toastify';
 import { BtnLoader } from '../../form/btn-loader/BtnLoader';
 import { GroupService } from '../../../service/service.group';
+import { IPerson } from '../../../model/model.person';
+import { PersonService } from '../../../service/service.person';
+import AsyncSelect from 'react-select/async';
 
 enum SAVE_MODE {
     CREATE = 'CREATE',
@@ -25,6 +28,15 @@ interface IState {
             value: string | undefined;
             isValid: boolean;
         };
+        person_id: {
+            value: string | undefined | undefined;
+            isValid: boolean;
+        };
+    };
+
+    person: {
+        value: { label: string, value: IPerson } | undefined | null; // IPerson | any,
+        // isValid: boolean
     };
 
     isFormValid: boolean;
@@ -49,6 +61,15 @@ class GroupSaveComponent extends BaseComponent<IProps, IState> {
                 value: undefined,
                 isValid: false,
             },
+            person_id: {
+                value: undefined,
+                isValid: true,
+            },
+        },
+
+        person: {
+            value: undefined,
+            // isValid: false,
         },
 
         isFormValid: false,
@@ -59,11 +80,13 @@ class GroupSaveComponent extends BaseComponent<IProps, IState> {
     }
 
     private _groupService = new GroupService();
+    private _personService = new PersonService();
 
     private group_id: string | undefined;
 
     componentDidMount() {
         this._groupService.setToken(this.props.token);
+        this._personService.setToken(this.props.token);
 
         if (this.props.match.path.includes('/group/:group_id/edit')) {
             this.setState({ ...this.state, saveMode: SAVE_MODE.EDIT });
@@ -81,11 +104,22 @@ class GroupSaveComponent extends BaseComponent<IProps, IState> {
     }
 
     async create() {
+        if (!this.state.isFormValid) return;
         this.setState({ ...this.state, createLoader: true });
 
-        const groupobj = {
-            group: this.state.group.groupname.value,
-        }
+        let groupobj: {};
+
+        this.state.person.value === undefined
+            ?
+            groupobj = {
+                title: this.state.group.groupname.value,
+            }
+            :
+            groupobj = {
+                title: this.state.group.groupname.value,
+                person_id: this.state.group.person_id.value
+            }
+
 
         let res = await this._groupService.create(groupobj).catch(error => {
             this.handleError({ error: error.response });
@@ -100,11 +134,21 @@ class GroupSaveComponent extends BaseComponent<IProps, IState> {
     }
 
     async update() {
+        if (!this.state.isFormValid) return;
         this.setState({ ...this.state, updateLoader: true });
 
-        const groupobj = {
-            group: this.state.group.groupname.value,
-        }
+        let groupobj: {};
+
+        this.state.person.value === undefined
+            ?
+            groupobj = {
+                title: this.state.group.groupname.value,
+            }
+            :
+            groupobj = {
+                title: this.state.group.groupname.value,
+                person_id: this.state.group.person_id.value
+            }
 
         let res = await this._groupService.update(groupobj, this.group_id!).catch(e => {
             this.handleError({ error: e.response });
@@ -147,6 +191,25 @@ class GroupSaveComponent extends BaseComponent<IProps, IState> {
         })
     }
 
+    handlePersonChange = (selectedPerson: { label: string, value: IPerson }) => {
+        let newperson = { ...selectedPerson };
+        let isValid = true;      // newperson = selectedPerson;
+        this.setState({
+            ...this.state, group: {
+                ...this.state.group, person_id: {
+                    value: newperson.value.id,
+                    isValid: isValid,
+                }
+            },
+            person: {
+                ...this.state.person,
+                value: newperson,
+                // isValid:true,
+            },
+            isFormValid: this.checkFormValidate(isValid, 'person_id')
+        })
+    }
+
     checkFormValidate(isValid: boolean, inputType: any): boolean {
         let valid = true;
         let groupObj: any = { ...this.state.group };
@@ -169,11 +232,54 @@ class GroupSaveComponent extends BaseComponent<IProps, IState> {
             ...this.state,
             group: {
                 groupname: { value: undefined, isValid: false },
+                person_id: { value: undefined, isValid: false },
             },
-
+            person: { value: null },
             isFormValid: false,
         })
     }
+
+    ////// request for person ////////
+
+    private personRequstError_txt: string = Localization.no_item_found;
+
+    async promiseOptions2(inputValue: any, callBack: any) {
+        let filter = undefined;
+        if (inputValue) {
+            filter = { person: inputValue };
+        }
+        let res: any = await this._personService.search(10, 0, filter).catch(err => {
+            let err_msg = this.handleError({ error: err.response, notify: false });
+            this.personRequstError_txt = err_msg.body;
+        });
+
+        if (res) {
+            let persons = res.data.result.map((ps: any) => {
+                return { label: this.getPersonFullName(ps), value: ps }
+            });
+            this.personRequstError_txt = Localization.no_item_found;
+            callBack(persons);
+        } else {
+            callBack();
+        }
+    }
+
+    private setTimeout_person_val: any;
+    debounce_300(inputValue: any, callBack: any) {
+        if (this.setTimeout_person_val) {
+            clearTimeout(this.setTimeout_person_val);
+        }
+        this.setTimeout_person_val = setTimeout(() => {
+            this.promiseOptions2(inputValue, callBack);
+        }, 1000);
+    }
+
+    select_noOptionsMessage(obj: { inputValue: string }) {
+        return this.personRequstError_txt;
+    }
+
+
+    /////////////////////////////////////
 
     ////// render ////////
     render() {
@@ -197,10 +303,23 @@ class GroupSaveComponent extends BaseComponent<IProps, IState> {
                                     <div className="col-md-3 col-sm-6">
                                         <Input
                                             onChange={(value, isValid) => this.handleInputChange(value, isValid, "groupname")}
-                                            label={Localization.group}
-                                            placeholder={Localization.group}
+                                            label={Localization.title}
+                                            placeholder={Localization.title}
                                             defaultValue={this.state.group.groupname.value}
                                             required
+                                        />
+                                    </div>
+                                    <div className="col-md-3 col-sm-6">
+                                        {/* <label >{Localization.person}{<span className="text-danger">*</span>}</label> */}
+                                        <label >{Localization.person}</label>
+                                        <AsyncSelect
+                                            placeholder={Localization.person}
+                                            cacheOptions
+                                            defaultOptions
+                                            value={this.state.person.value}
+                                            loadOptions={(inputValue, callback) => this.debounce_300(inputValue, callback)}
+                                            noOptionsMessage={(obj) => this.select_noOptionsMessage(obj)}
+                                            onChange={(selectedPerson: any) => this.handlePersonChange(selectedPerson)}
                                         />
                                     </div>
                                 </div>

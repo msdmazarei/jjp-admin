0eef238da1e74515f9c056d106599834ba5f8813
ali kashefi <kashefi.eng@gmail.com>
2019-10-08 16:24:37 +0330

    add modal for permission add to group

--- src/component/group/GroupManage/GroupManage.tsx
+++ src/component/group/GroupManage/GroupManage.tsx
@@ -16,7 +16,9 @@ import 'moment/locale/fa';
 import 'moment/locale/ar';
 import moment from 'moment';
 import moment_jalaali from 'moment-jalaali';
+import AsyncSelect from 'react-select/async';
 import { GroupService } from "../../../service/service.group";
+import { PermissionService } from "../../../service/service.permission";
 
 //// start define IProps ///
 
@@ -36,7 +38,7 @@ interface IFilterUser {
     value: string | undefined;
     isValid: boolean;
   };
-} 
+}
 
 //// end define IFilterUser ///
 
@@ -49,14 +51,20 @@ interface IState {
   pager_offset: number;
   pager_limit: number;
   removeModalShow: boolean;
+  addPermissionModalShow: boolean;
   prevBtnLoader: boolean;
   nextBtnLoader: boolean;
   setRemoveLoader: boolean;
+  setAddPermissionLoader: boolean;
   isSearch: boolean;
   searchVal: string | undefined;
   filter: IFilterUser,
   filterSearchBtnLoader: boolean;
   tableProcessLoader: boolean;
+  permissions: {
+    value: any[] | null,
+    isValid: boolean
+  };
 }
 
 //// end define IState ///
@@ -92,12 +100,22 @@ class GroupManageComponent extends BaseComponent<IProps, IState>{
         },
       ],
       actions: [
-        { text: <i title={Localization.remove} className="fa fa-trash text-danger"></i>,
-         ac_func: (row: any) => { this.onShowRemoveModal(row) },
-         name:Localization.remove },
-        { text: <i title={Localization.update} className="fa fa-pencil-square-o text-primary"></i>,
-         ac_func: (row: any) => { this.updateRow(row) },
-         name:Localization.update },
+        {
+          text: <i title={Localization.remove} className="fa fa-trash text-danger"></i>,
+          ac_func: (row: any) => { this.onShowRemoveModal(row) },
+          name: Localization.remove
+        },
+        {
+          text: <i title={Localization.permission} className="fa fa-universal-access text-info"></i>,
+          ac_func: (row: any) => { this.onShowAddPermissionModal(row) },
+          name: Localization.permission
+        },
+
+        {
+          text: <i title={Localization.update} className="fa fa-pencil-square-o text-primary"></i>,
+          ac_func: (row: any) => { this.updateRow(row) },
+          name: Localization.update
+        },
       ]
     },
     filter: {
@@ -112,16 +130,26 @@ class GroupManageComponent extends BaseComponent<IProps, IState>{
     prevBtnLoader: false,
     nextBtnLoader: false,
     removeModalShow: false,
+    addPermissionModalShow:true,
     setRemoveLoader: false,
+    setAddPermissionLoader:false,
     isSearch: false,
     searchVal: undefined,
     filterSearchBtnLoader: false,
     tableProcessLoader: false,
+    permissions: {
+      value: null,
+      isValid: false,
+    },
   }
 
   selectedGroup: any | undefined;
+  selectedGroupForPermission: any | undefined;
+  private permissionRequstError_txt: string = Localization.no_item_found;
+  private setTimeout_permission_val: any;
 
   private _groupService = new GroupService();
+  private _permissionService = new PermissionService();
 
   constructor(props: IProps) {
     super(props);
@@ -131,7 +159,7 @@ class GroupManageComponent extends BaseComponent<IProps, IState>{
   componentDidMount() {
     this.setState({
       ...this.state,
-      tableProcessLoader:true
+      tableProcessLoader: true
     })
     this.fetchGroup();
   }
@@ -150,12 +178,12 @@ class GroupManageComponent extends BaseComponent<IProps, IState>{
   /// start for all function for request ///
 
   async fetchGroup() {
-    this.setState({...this.state,tableProcessLoader: true});
+    this.setState({ ...this.state, tableProcessLoader: true });
     let res = await this._groupService.search(
-      this.state.pager_limit, 
+      this.state.pager_limit,
       this.state.pager_offset,
       this.getFilter()
-      ).catch(error => {
+    ).catch(error => {
       this.handleError({ error: error.response });
       this.setState({
         ...this.state,
@@ -180,7 +208,7 @@ class GroupManageComponent extends BaseComponent<IProps, IState>{
     }
   }
 
-  async onRemoveUser(group_id: string) {
+  async onRemoveGroup(group_id: string) {
     this.setState({ ...this.state, setRemoveLoader: true });
     let res = await this._groupService.remove(group_id).catch(error => {
       this.handleError({ error: error.response });
@@ -229,7 +257,7 @@ class GroupManageComponent extends BaseComponent<IProps, IState>{
             <button className="btn btn-light shadow-default shadow-hover" onClick={() => this.onHideRemoveModal()}>{Localization.close}</button>
             <BtnLoader
               btnClassName="btn btn-danger shadow-default shadow-hover"
-              onClick={() => this.onRemoveUser(selectedGroup.id)}
+              onClick={() => this.onRemoveGroup(selectedGroup.id)}
               loading={this.state.setRemoveLoader}
             >
               {Localization.remove}
@@ -241,6 +269,136 @@ class GroupManageComponent extends BaseComponent<IProps, IState>{
   }
 
   /// end remove functions and render ///
+
+
+   // start add permission modal function define ////////
+
+  onShowAddPermissionModal(group: any) {
+    this.selectedGroupForPermission = group;
+    this.setState({ ...this.state, addPermissionModalShow: true });
+  }
+
+  onHideAddPermissionModal() {
+    this.selectedGroupForPermission = undefined;
+    this.setState({
+      ...this.state,
+      permissions: { value: null, isValid: false },
+      addPermissionModalShow: false
+    });
+
+  }
+
+  debounce_300(inputValue: any, callBack: any) {
+    if (this.setTimeout_permission_val) {
+      clearTimeout(this.setTimeout_permission_val);
+    }
+    this.setTimeout_permission_val = setTimeout(() => {
+      this.promiseOptions2(inputValue, callBack);
+    }, 1000);
+  }
+
+  async promiseOptions2(inputValue: any, callBack: any) {
+    let filter = undefined;
+    if (inputValue) {
+      filter = { permission: inputValue };
+    }
+    let res: any = await this._permissionService.search(10, 0, filter).catch(err => {
+      let err_msg = this.handleError({ error: err.response, notify: false });
+      this.permissionRequstError_txt = err_msg.body;
+    });
+
+    if (res) {
+      let groups = res.data.result.map((ps: any) => {
+        return { label: ps.permission, value: ps }
+      });
+      this.permissionRequstError_txt = Localization.no_item_found;
+      callBack(groups);
+    } else {
+      callBack();
+    }
+  }
+
+  select_noOptionsMessage(obj: { inputValue: string }) {
+    return this.permissionRequstError_txt;
+  }
+
+  handleSelectInputChange(value: any[]) {
+    let isValid;
+    if (!value || !value.length) {
+      isValid = false;
+    } else {
+      isValid = true;
+    }
+    this.setState({
+      ...this.state,
+      permissions: {
+        value: value || [], isValid: isValid
+      }
+    })
+  }
+
+  async onAddpermissionToGroup(user_id: string) {
+    this.setState({ ...this.state, setAddPermissionLoader: true });
+    let res = await this._permissionService.remove(user_id).catch(error => {
+      this.handleError({ error: error.response });
+      this.setState({ ...this.state, setAddPermissionLoader: false });
+    });
+    if (res) {
+      this.setState({ ...this.state, setAddPermissionLoader: false });
+      this.apiSuccessNotify();
+      this.fetchGroup();
+      this.onHideRemoveModal();
+    }
+  }
+
+  render_AddPermissionToGroup_modal(selectedGroupForPermission: any) {
+    // if (!this.selectedGroupForPermission || !this.selectedGroupForPermission.id) return;
+    return (
+      <>
+        <Modal size='xl' show={this.state.addPermissionModalShow} onHide={() => this.onHideAddPermissionModal()}>
+          <Modal.Header>
+            <h2 className='text-bold text-dark text-center w-100'>افزودن دسترسی</h2>
+          </Modal.Header>
+          <Modal.Body>
+            <p className="delete-modal-content">
+              <span className="text-muted">
+                {Localization.name}{" "}{Localization.group}:&nbsp;
+              </span>
+              {/* {this.selectedGroupForPermission.name} {this.selectedGroupForPermission.username} */}
+            </p>
+            <div className="row">
+              <div className="col-6">
+                <label >{Localization.permission}</label>
+                <AsyncSelect
+                  isMulti
+                  placeholder={Localization.permission}
+                  cacheOptions
+                  defaultOptions
+                  value={this.state.permissions.value}
+                  loadOptions={(inputValue:any, callback:any) => this.debounce_300(inputValue, callback)}
+                  noOptionsMessage={(obj:any) => this.select_noOptionsMessage(obj)}
+                  onChange={(selectedPerson: any) => this.handleSelectInputChange(selectedPerson)}  
+                />
+              </div>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <button className="btn btn-light shadow-default shadow-hover" onClick={() => this.onHideAddPermissionModal()}>{Localization.close}</button>
+            <BtnLoader
+              btnClassName="btn btn-success shadow-default shadow-hover"
+              onClick={() => this.onAddpermissionToGroup(selectedGroupForPermission.id)}
+              loading={this.state.setAddPermissionLoader}
+              disabled={!this.state.permissions.isValid}
+            >
+              {Localization.create}
+            </BtnLoader>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+
+  // end add permission modal function define ////////
 
 
   /// start search functions and onchange handler ///
@@ -285,7 +443,7 @@ class GroupManageComponent extends BaseComponent<IProps, IState>{
   }
 
   private _filter: IFilterUser = {
-    group : { value: undefined, isValid: true },
+    group: { value: undefined, isValid: true },
   };
   isFilterEmpty(): boolean {
     if (this._filter.group.value) {
@@ -507,6 +665,7 @@ class GroupManageComponent extends BaseComponent<IProps, IState>{
           </div>
         </div>
         {this.render_delete_modal(this.selectedGroup)}
+        {this.render_AddPermissionToGroup_modal(this.selectedGroupForPermission)}
         <ToastContainer {...this.getNotifyContainerConfig()} />
       </>
     );

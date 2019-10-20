a9b5be2bf73e1306ec69ab0cd0e83964893ed853
ali kashefi <kashefi.eng@gmail.com>
2019-10-20 16:20:59 +0330

    function modified

--- src/component/form/app-datePicker/AppDatePicker.tsx
+++ src/component/form/app-datePicker/AppDatePicker.tsx
@@ -5,8 +5,7 @@ import { TInternationalization } from '../../../config/setup';
 import { BaseComponent } from '../../_base/BaseComponent';
 import { redux_state } from '../../../redux/app_state';
 // import { DateTimeInput, DateTimeInputSimple, DateInput, DateInputSimple } from 'react-hichestan-datetimepicker';
-import { DateInput } from 'react-hichestan-datetimepicker';
-
+import { DateInput, DateTimeInput } from 'react-hichestan-datetimepicker';
 
 interface IProps {
     history?: History;
@@ -19,47 +18,66 @@ interface IProps {
     gregorian?: boolean;
     autoOk?: boolean;
     disable?: boolean;
-}
+};
 
-class AppDatePickerComponent extends BaseComponent<IProps> {
-    
+interface IState {
+    value1: string;
+    value1_formatted: string;
+    is_touch: boolean;
+};
+
+class AppDatePickerComponent extends BaseComponent<IProps, IState> {
+
     state = {
         value1: '',
         value1_formatted: '',
+        is_touch: false,
     };
 
-    componentWillReceiveProps(nextProps: IProps){
-        if(nextProps.value !== this.props.value){
-            if(nextProps.value !==undefined){
-                this.setState({
-                    ...this.state,
-                    value1: new Date(nextProps.value * 1000).toISOString(),
-                })
-            }else{
-                this.setState({
-                    ...this.state,
-                    value1:'',
-                })
-            }
+    componentWillReceiveProps(nextProps: IProps) {
+        if (nextProps.value !== this.props.value && nextProps.value !== undefined && nextProps === undefined) {
+            return
         }
-
+        if (nextProps.value !== this.props.value && nextProps.value !== undefined) {
+            this.setState({
+                ...this.state,
+                value1: new Date(nextProps.value * 1000).toISOString(),
+            });
+        }
+        if (nextProps.value === undefined && this.props.value === undefined) {
+            this.setState({
+                ...this.state,
+                value1: '',
+            });
+        }
     }
 
     handleChange(event: any) {
         if (this.props.disable) {
             return;
         }
-   
-        const newState :any={};
+        if (this.state.is_touch && this.props.value === undefined) {
+            this.setState({
+                value1: '',
+                value1_formatted: '',
+                is_touch: false,
+            })
+            return;
+        }
+        const newState: any = {};
         const t = event.target;
         newState[t.name] = t.value;
-        newState[t.name+'_formatted'] = t.formatted ? t.formatted : '';
+        newState[t.name + '_formatted'] = t.formatted ? t.formatted : '';
         this.setState(newState);
- 
+        this.setState({
+            ...this.state,
+            is_touch: true,
+        })
+
         const val = event.target.value;
         const ts = new Date(val).getTime() / 1000;
-        if(this.props.onChange){
-            this.props.onChange(ts , this.validationFunc() )
+        if (this.props.onChange) {
+            this.props.onChange(ts, this.validationFunc())
         }
 
     }
@@ -73,7 +91,6 @@ class AppDatePickerComponent extends BaseComponent<IProps> {
             <div className={this.props.disable ? "row form-group app-datepicker px-3 all-event-disable" : "row form-group app-datepicker px-3"}   >
                 <label htmlFor="">{this.props.lable ? this.props.lable : ""}</label>
                 <DateInput
-                    // value={this.props.value === undefined ? '' : new Date(this.props.value * 1000).toString()}
                     value={this.state.value1}
                     name={'value1'}
                     autoOk={this.props.autoOk}

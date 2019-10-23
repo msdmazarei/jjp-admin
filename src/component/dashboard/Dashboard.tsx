7daefa73c1c97731829422293645bdc30728dd95
ali kashefi <kashefi.eng@gmail.com>
2019-10-23 20:47:07 +0330

    modified

--- src/component/dashboard/Dashboard.tsx
+++ src/component/dashboard/Dashboard.tsx
@@ -9,6 +9,7 @@ import { TInternationalization } from "../../config/setup";
 import { Localization } from "../../config/localization/localization";
 import { AppWidgets } from "../tool/appWidgets/appWidgets";
 import { TReport, reportListMapCmp } from "../tool/reports/ReportUtils";
+import { DashboardReportsManageModal } from "./DashboardReportsManage"
 
 export interface IProps {
   history: History;
@@ -18,12 +19,15 @@ export interface IProps {
 
 interface IState {
   report_cmp_list: string[];
+  report_cmp_status: string[];
+  reportManagerModalShow: boolean;
 }
 
 class DashboardComponent extends BaseComponent<IProps, IState> {
   state = {
     report_cmp_list: [],
-
+    report_cmp_status: ['true', 'true', 'true', 'true', 'true', 'true'],
+    reportManagerModalShow: false,
   }
   /// end of state
 
@@ -39,39 +43,101 @@ class DashboardComponent extends BaseComponent<IProps, IState> {
 
   grtUserReport(uer_id: string): Promise<TReport[]> {
     return new Promise((res, rej) => {
-      res(["newst_comment", "best_sells_chart",
+      res([
+        "newst_comment",
+        "best_sells_chart",
         "last_sell_with_type",
         "year_sell_with_month",
         "Compear_publisher_sells",
-        'store_customer_performance'
+        "store_customer_performance"
       ])
     })
   }
 
+  reports_status_handler(index: number) {
+    const array: string[] = this.state.report_cmp_status;
+    array[index] = 'false';
+    this.setState({
+      ...this.state,
+      report_cmp_status: array,
+    })
+  }
+
+  render_reports_func() {
+    return <div className="row">
+      {
+        this.state.report_cmp_list.map((report: TReport, r_index:number) => {
+          const Cmpname = reportListMapCmp[report];
+          const status: string = this.state.report_cmp_status[r_index];
+          if (status === 'true') {
+            return (
+              <Fragment key={r_index}>
+                <div className="col-12 col-xl-6 mb-3 widget-wrapper">
+                  <AppWidgets
+                    onClose={() => this.reports_status_handler(r_index)}
+                  >
+                    <Cmpname />
+                  </AppWidgets>
+                </div>
+              </Fragment>
+            )
+          } if (status === 'false') {
+            return (
+              <Fragment key={r_index}>
+                <div className="col-12 col-xl-6 mb-3 widget-wrapper d-none">
+                  <AppWidgets
+                    onClose={() => this.reports_status_handler(r_index)}
+                  >
+                    <Cmpname />
+                  </AppWidgets>
+                </div>
+              </Fragment>
+            )
+          }
+        })
+      }
+    </div>
+  }
+
+  reportManagerModalShow_handler() {
+    this.setState({
+      ...this.state,
+      reportManagerModalShow: true,
+    })
+  }
+
+  reportManagerModal_seter(newList: string[]) {
+    this.setState({
+      ...this.state,
+      reportManagerModalShow: false,
+      report_cmp_status : newList,
+    })
+  }
+
+
   render() {
     return (
       <div className="content">
+        <div
+          className='bg-dark p-1 dashboard-report-manage-btn-wraper'
+        >
+          <i
+            title={Localization.settings}
+            className="fa fa-cog fa-2x dashboard-report-manage-btn"
+            onClick={() => this.reportManagerModalShow_handler()}
+          ></i>
+        </div>
         <div className="row">
           <div className="col-12">
             <h2>{Localization.dashboard}</h2>
           </div>
         </div>
-        <div className="row">
-          {
-            this.state.report_cmp_list.map((report: TReport, r_index) => {
-              const Cmpname = reportListMapCmp[report];
-              return (
-                <Fragment key={r_index}>
-                  <div className="col-12 col-xl-6 mb-3">
-                    <AppWidgets>
-                      <Cmpname />
-                    </AppWidgets>
-                  </div>
-                </Fragment>
-              )
-            })
-          }
-        </div>
+        {this.render_reports_func()}
+        <DashboardReportsManageModal
+          firstList={this.state.report_cmp_status}
+          modalShow={this.state.reportManagerModalShow}
+          onHide={(newList) => this.reportManagerModal_seter(newList)}
+        />
       </div>
     );
   }

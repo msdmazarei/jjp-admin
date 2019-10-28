3f502a46f7d8bd7f58ad424f70dd9a8a82a924de
ali kashefi <kashefi.eng@gmail.com>
2019-10-28 20:43:23 +0330

    add new check access function

--- src/service/service.access.ts
+++ src/service/service.access.ts
@@ -7,6 +7,41 @@ export class AccessService extends BaseService {
         const user = Store2.getState().logged_in_user;
         if (!user) return false;
         return user.permissions.includes(ac);
+    };
+
+    static checkAllAccess( array : string[]): boolean {
+        const User = Store2.getState().logged_in_user;
+        if(array.length === 0 || !User ){return false}
+        let valid = true;
+        for (let i = 0; i < array.length; i++) {
+            valid = valid &&  User.permissions.includes(array[i]);
+            if(!valid){
+                break;
+            }
+        }
+        return valid;
     }
 
+    static checkOneOFAllAccess( array : string[]): boolean {
+        const User = Store2.getState().logged_in_user;
+        if(array.length === 0 || !User ){return false}
+        let valid = false;
+        for (let i = 0; i < array.length; i++) {
+            valid = User.permissions.includes(array[i]);
+            if(valid){
+                break;
+            }
+        }
+        return valid;
+    }
+    
+    static checkBookCreateIcon():boolean{
+        const user = Store2.getState().logged_in_user;
+        if (!user) return false;
+        if (user.permissions.includes('BOOK_ADD_PREMIUM') || user.permissions.includes('BOOK_ADD_PRESS')){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }

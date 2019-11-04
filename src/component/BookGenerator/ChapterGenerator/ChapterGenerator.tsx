126e3444c84ac26b0b42a8e1f509155cc2906175
ali kashefi <kashefi.eng@gmail.com>
2019-11-04 21:02:26 +0330

    book chapter generator modified

--- src/component/BookGenerator/ChapterGenerator/ChapterGenerator.tsx
+++ src/component/BookGenerator/ChapterGenerator/ChapterGenerator.tsx
@@ -1,124 +1,182 @@
-import React from 'react';
-// import { History } from 'history';
+import React, { Fragment } from 'react';
 import { BaseComponent } from '../../_base/BaseComponent';
 import { TInternationalization } from '../../../config/setup';
 import { MapDispatchToProps, connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { redux_state } from '../../../redux/app_state';
-// import { IToken } from '../../../model/model.token';
-// import { ToastContainer } from 'react-toastify';
-import { Input } from '../../form/input/Input';
-import { Localization } from '../../../config/localization/localization';
-import { Book_body } from '../BookGenerator/BookGenerator';
-import SortableTree, { toggleExpandedForAll } from 'react-sortable-tree';
+import { Book_body, Book_children } from '../BookGenerator/BookGenerator';
+import { AppGuid } from '../../../asset/script/guid';
 import { BodyGenerator } from '../BodyGenerator/BodyGenerator';
 
-interface Body_children_converted {
-    id: string;
-    title: JSX.Element;
-    type: string;
-    text: string;
-    control: string;
-}
 
 interface IProps {
-    // match: any;
-    // history: History;
+    match?: any;
+    history?: History;
     internationalization: TInternationalization;
-    title?: string;
-    id: string;
-    onTitleChange: (value: any, isValid: boolean, id: string) => void;
-    addChildAfter: (id: string) => void;
-    addChildBefore: (id: string) => void;
-    body: Book_body[];
-    treeDataChange: (treeData : any) => void;
+    booktitle: string;
+    bookContent: Book_children[];
+    onChangeBook: (bookContent: Book_children[]) => void;
 }
 
 interface IState {
-
+    bookTitle: string;
+    bookContent: Book_children[];
 }
 
 class ChapterGeneratorComponent extends BaseComponent<IProps, IState> {
-
-    addChildToBefore(id: string) {
-        debugger;
+    state = {
+        bookTitle: '',
+        bookContent: [],
     }
 
-    addChildToAfter(id: string) {
-        debugger;
-    }
-
-    bodyChanged(value: any, isValid: boolean, id: string) {
-        if(isValid === false) return;
-        let data = this._convertedData;
-        data.forEach(element => {
-            if(element.id === id){
-                return element.text === value;
-            }
+    componentDidMount() {
+        this.setState({
+            ...this.state,
+            bookTitle: this.props.booktitle,
+            bookContent: this.props.bookContent,
         });
-        this._convertedData = data;
     }
 
-    convert_bodyChildren(list: any[]): void {
-        for (let i = 0; i < list.length; i++) {
-            let body = list[i];
-            body.title = <>
-                <BodyGenerator
-                    id={body.id}
-                    type={body.type}
-                    text={body.text ? body.text : undefined}
-                    control={body.control ? body.control : undefined}
-                    addChildAfter={(id) => this.addChildToAfter(id)}
-                    addChildBefore={(id) => this.addChildToBefore(id)}
-                    onbodyChange={(value, isValid, id) => this.bodyChanged(value, isValid, id)}
-                />
-            </>;
-            body.expanded = true; // todo _DELETE_ME
-        }
-    }
-    _convertedData: Body_children_converted[] = []
-    getConvertedData() {
-        // debugger;
-        let val: any = [...this.props.body];
-        this.convert_bodyChildren(val)
-        return this._convertedData = [...val];
+    componentWillReceiveProps(nextProps: IProps) {
+        if (nextProps === this.props) return;
+        this.setState({
+            ...this.state,
+            bookTitle: nextProps.booktitle,
+            bookContent: nextProps.bookContent,
+        });
     }
 
-    onTitleChange(value: any, isValid: boolean) {
-        this.props.onTitleChange(value, isValid, this.props.id);
+    passNewBookContentToProps(){
+        this.props.onChangeBook(this.state.bookContent)
+    }
+
+    addChapter() {
+        let newBookContent: Book_children[] = this.state.bookContent.length === 0 ? [] : this.state.bookContent;
+        const newId: string = AppGuid.generate();
+        let newChild: Book_children = { id: newId, title: '', body: [], children: [] };
+        newBookContent.push(newChild);
+        this.setState({
+            ...this.state,
+            bookContent: newBookContent,
+        },() => this.passNewBookContentToProps())
+    }
+
+    addChapterBefore(id: string) {
+        let newBookContent: Book_children[] = this.state.bookContent.length === 0 ? [] : this.state.bookContent;
+        let Obj: Book_children | undefined = newBookContent.find(newBookContent => newBookContent.id === id);
+        if (Obj === undefined) return;
+        let index: number = newBookContent.indexOf(Obj);
+        const newId: string = AppGuid.generate();
+        let newChild: Book_children = { id: newId, title: '', body: [], children: [] };
+        newBookContent.splice(index, 0, newChild);
+        this.setState({
+            ...this.state,
+            bookContent: newBookContent,
+        },() => this.passNewBookContentToProps())
+    }
+
+    addChapterAfter(id: string) {
+        let newBookContent: Book_children[] = this.state.bookContent.length === 0 ? [] : this.state.bookContent;
+        let Obj: Book_children | undefined = newBookContent.find(newBookContent => newBookContent.id === id);
+        if (Obj === undefined) return;
+        let index: number = newBookContent.indexOf(Obj);
+        const newId: string = AppGuid.generate();
+        let newChild: Book_children = { id: newId, title: '', body: [], children: [] };
+        newBookContent.splice((index + 1), 0, newChild);
+        this.setState({
+            ...this.state,
+            bookContent: newBookContent,
+        },() => this.passNewBookContentToProps())
+    }
+
+    addNewSubChapterFromBodyGeneratorComponent(id: string) {
+        let newBookContent: Book_children[] = this.state.bookContent.length === 0 ? [] : this.state.bookContent;
+        let Obj: Book_children | undefined = newBookContent.find(newBookContent => newBookContent.id === id);
+        if (Obj === undefined) return;
+        let index: number = newBookContent.indexOf(Obj);
+        const newId: string = AppGuid.generate();
+        let newChild: Book_children = { id: newId, title: '', body: [], children: [] };
+        newBookContent[index].children.push(newChild);
+        this.setState({
+            ...this.state,
+            bookContent: newBookContent,
+        },() => this.passNewBookContentToProps());
+    }
+
+    updateChapterBody(newbody: Book_body[], title: string, id: string) {
+        let newBookContent: Book_children[] = this.state.bookContent.length === 0 ? [] : this.state.bookContent;
+        if(newBookContent.length === 0 ) return;
+        let Obj: Book_children | undefined = newBookContent.find(newBookContent => newBookContent.id === id);
+        if (Obj === undefined) return;
+        let index: number = newBookContent.indexOf(Obj);
+        newBookContent[index].title = title;
+        newBookContent[index].body = newbody;
+        this.setState({
+            ...this.state,
+            bookContent: newBookContent,
+        },() => this.passNewBookContentToProps());
+    }
+
+    chapters_render(array: Book_children[]) {
+        return <>
+            {
+                array.map((item: Book_children, i: number) => (
+                    <Fragment key={i}>
+                        <div className="col-12">
+                            <i className="fa fa-pencil" onClick={() => this.addChapterBefore(item.id)}></i>
+                            <BodyGenerator
+                                id={item.id}
+                                title={item.title ? item.title : ''}
+                                body={(item.body === undefined || item.body.length === 0) ? [] : item.body}
+                                onBodyChange={(newbody: Book_body[], title: string, id: string) => this.updateChapterBody(newbody, title, id)}
+                                addSubChapter={(id: string) => this.addNewSubChapterFromBodyGeneratorComponent(id)}
+                            />
+                            {
+                                item.children
+                                    ?
+                                    item.children.length > 0
+                                        ?
+                                        <div>
+                                            {
+                                                this.chapters_render(item.children)
+                                            }
+                                        </div>
+                                        :
+                                        undefined
+                                    :
+                                    undefined
+                            }
+                            <i className="fa fa-pencil" onClick={() => this.addChapterAfter(item.id)}></i>
+                        </div>
+                    </Fragment>
+                ))
+            }
+        </>
     }
 
     render() {
         return (
-            <>
-                <i className="fa fa-plus fa-2x" onClick={() => this.props.addChildBefore(this.props.id)}></i>
-                <Input
-                    required
-                    label={Localization.title}
-                    placeholder={Localization.title}
-                    defaultValue={this.props.title}
-                    onChange={(value: any, isValid: boolean) => this.onTitleChange(value, isValid)}
-                ></Input>
-                <i className="fa fa-plus fa-2x" onClick={() => this.props.addChildAfter(this.props.id)}></i>
-                {/* <SortableTree
-                    treeData={this.getConvertedData()}
-                    onChange={(treeData : any) => this.props.treeDataChange(treeData)}
-                    rowDirection='rtl'
-                    onMoveNode={(node: any, treeIndex: any, path: any) =>
-                        global.console.debug(
-                            'node:',
-                            node,
-                            'treeIndex:',
-                            treeIndex,
-                            'path:',
-                            path
-                        )
+            <div>
+                <div className="row">
+                    <div className="col-12">
+                        <div className="text-center">
+                            {this.state.bookTitle}
+                        </div>
+                        <div>
+                            <i className="fa fa-indent" onClick={() => this.addChapter()}></i>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    {
+                        this.state.bookContent.length === 0
+                            ?
+                            undefined
+                            :
+                            this.chapters_render(this.state.bookContent)
                     }
-                    canDrag={(node: any) => !node.noDragging}
-                    canDrop={(nextParent: any) => !nextParent || !nextParent.noChildren}
-                    isVirtualized={true}
-                /> */}
-            </>
+                </div>
+            </div>
         )
     }
 }

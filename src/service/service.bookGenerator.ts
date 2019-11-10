8373c56002e38b187bf78746bab04422b8a0af89
ali kashefi <47456857+alikashefi1989@users.noreply.github.com>
2019-11-11 00:24:28 +0330

    bookGenerator service added

---
+++ src/service/service.bookGenerator.ts
@@ -0,0 +1,31 @@
+// import Axios from "axios";
+import { IBook } from "../model/model.book";
+import { IAPI_Response, IAPI_ResponseList, BaseService } from "./service.base";
+
+export class BookGeneratorService extends BaseService {
+    // instance = Axios.create({
+    //     baseURL: '/api',
+    //     headers: { 'authorization': 'Bearer 0be2e4cb-88b3-44a6-aeff-4eeb96e411c6' }
+    // });
+
+    create(book: any) {
+        return this.axiosTokenInstance.post('/books', book);
+    }
+
+    search(limit: number, offset: number, filter?: Object): Promise<IAPI_ResponseList<IBook>> {
+        return this.axiosTokenInstance.post(`/books/_search`, { limit, offset, filter });
+    }
+
+    remove(bookId: string) {
+        return this.axiosTokenInstance.delete(`/books/${bookId}`);
+    }
+
+    byId(book_id: string): Promise<IAPI_Response<IBook>> {
+        return this.axiosTokenInstance.get(`/books/${book_id}`);
+    }
+
+    update(book: any, id: string) {
+        return this.axiosTokenInstance.put(`/books/${id}`, book);
+    }
+
+}

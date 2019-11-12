5f22ad6e38e5b8b3e0b945cd8a7c504ccf1c538f
ali kashefi <kashefi.eng@gmail.com>
2019-11-12 20:46:43 +0330

    offset change to skip in search function

--- src/service/service.book.ts
+++ src/service/service.book.ts
@@ -12,8 +12,8 @@ export class BookService extends BaseService {
         return this.axiosTokenInstance.post('/books', book);
     }
 
-    search(limit: number, offset: number, filter?: Object): Promise<IAPI_ResponseList<IBook>> {
-        return this.axiosTokenInstance.post(`/books/_search`, { limit, offset, filter });
+    search(limit: number, skip: number, filter?: Object): Promise<IAPI_ResponseList<IBook>> {
+        return this.axiosTokenInstance.post(`/books/_search`, { limit, skip, filter });
     }
 
 

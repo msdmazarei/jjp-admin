cbc49d0c06f5b962e4decd65064ef6c2fb408740
ali kashefi <kashefi.eng@gmail.com>
2019-11-26 20:43:03 +0330

    add cmp validation boolean

--- src/component/form/app-rangepicker/AppRangePicker.tsx
+++ src/component/form/app-rangepicker/AppRangePicker.tsx
@@ -12,7 +12,7 @@ interface IProps {
     internationalization: TInternationalization;
     from: number | undefined;
     to: number | undefined;
-    onChange: (from: number | undefined, from_isValid: boolean , to: number | undefined, to_isValid: boolean) => void;
+    onChange: (from: number | undefined, from_isValid: boolean, to: number | undefined, to_isValid: boolean, is_valid: boolean) => void;
     label: string;
     time?: boolean;
 };
@@ -29,6 +29,8 @@ class AppRangePickerComponent extends BaseComponent<IProps, IState> {
         to: undefined,
     };
 
+    cmp_has_error: boolean = false;
+
     componentDidMount() {
         this.setState({
             from: this.props.from,
@@ -36,36 +38,80 @@ class AppRangePickerComponent extends BaseComponent<IProps, IState> {
         })
     }
 
-    componentWillReceiveProps(nextProps : IProps){
-        if(nextProps === this.props){
+    componentWillReceiveProps(nextProps: IProps) {
+        if (nextProps === this.props) {
             return;
-        }else{
+        } else {
             this.setState({
-                from : nextProps.from,
-                to : nextProps.to,
+                from: nextProps.from,
+                to: nextProps.to,
             })
+        }
+        if(nextProps.from === undefined && nextProps.to === undefined){
+            this.cmp_has_error = false;
         }
     }
 
     handleChange(timestamp: number | undefined, isValid: boolean, type: string) {
+        let cmp_validation: boolean = false;
         if (isValid === false) {
             return;
         }
         if (type === 'from') {
-            let from_isValid : boolean = timestamp === undefined ? false : true;
-            let to_isValid : boolean = this.state.to === undefined ? false : true;
+            if (this.state.to === undefined) {
+                if (timestamp === undefined) {
+                    cmp_validation = false;
+                    this.cmp_has_error = true;
+                } else {
+                    cmp_validation = true;
+                    this.cmp_has_error = false;
+                }
+            } else {
+                if (timestamp === undefined) {
+                    cmp_validation = true;
+                    this.cmp_has_error = false;
+                } else if (timestamp <= this.state.to!) {
+                    cmp_validation = true;
+                    this.cmp_has_error = false;
+                } else {
+                    cmp_validation = false;
+                    this.cmp_has_error = true;
+                }
+            }
+            let from_isValid: boolean = timestamp === undefined ? false : true;
+            let to_isValid: boolean = this.state.to === undefined ? false : true;
             this.setState({
                 ...this.state,
                 from: timestamp,
-            }, () => this.props.onChange(this.state.from,from_isValid,this.state.to,to_isValid))
+            }, () => this.props.onChange(this.state.from, from_isValid, this.state.to, to_isValid, cmp_validation))
         }
         if (type === 'to') {
-            let from_isValid : boolean = this.state.from === undefined ? false : true;
-            let to_isValid : boolean = timestamp === undefined ? false : true;
+            if (this.state.from === undefined) {
+                if (timestamp === undefined) {
+                    cmp_validation = false;
+                    this.cmp_has_error = true;
+                } else {
+                    cmp_validation = true;
+                    this.cmp_has_error = false;
+                }
+            } else {
+                if (timestamp === undefined) {
+                    cmp_validation = true;
+                    this.cmp_has_error = false;
+                } else if (timestamp >= this.state.from!) {
+                    cmp_validation = true;
+                    this.cmp_has_error = false;
+                } else {
+                    cmp_validation = false;
+                    this.cmp_has_error = true;
+                }
+            }
+            let from_isValid: boolean = this.state.from === undefined ? false : true;
+            let to_isValid: boolean = timestamp === undefined ? false : true;
             this.setState({
                 ...this.state,
                 to: timestamp,
-            }, () => this.props.onChange(this.state.from,from_isValid,this.state.to,to_isValid))
+            }, () => this.props.onChange(this.state.from, from_isValid, this.state.to, to_isValid, cmp_validation))
         }
     }
 
@@ -84,10 +130,19 @@ class AppRangePickerComponent extends BaseComponent<IProps, IState> {
     }
 
     reseter2() {
+        this.cmp_has_error = false;
         this.setState({
             from: undefined,
             to: undefined,
-        }, () => this.props.onChange(undefined,false,undefined,false));
+        }, () => this.props.onChange(undefined, false, undefined, false, false));
+    }
+
+    error_msg_handler(has_error: boolean) {
+        if (has_error === true) {
+            return <div className="select-feedback mt-n2 px-1">{Localization.selected_range_is_not_valid}</div>
+        } else {
+            return
+        }
     }
 
     render() {
@@ -153,6 +208,7 @@ class AppRangePickerComponent extends BaseComponent<IProps, IState> {
                         </i>
                     </div>
                 </div>
+                {this.error_msg_handler(this.cmp_has_error)}
             </>
         )
     }

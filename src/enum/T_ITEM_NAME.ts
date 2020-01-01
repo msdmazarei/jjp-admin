ca68a72c63519188675470d63ef49a5c3c695863
ali kashefi <kashefi.eng@gmail.com>
2020-01-01 17:28:48 +0330

    modified

--- src/enum/T_ITEM_NAME.ts
+++ src/enum/T_ITEM_NAME.ts
@@ -1,28 +1,55 @@
 export enum T_ITEM_NAME {
     dashboard = 'dashboard',
+    // start book
     bookManage = 'bookManage',
     bookSave = 'bookSave',
     bookEdit = 'bookEdit',
+    // end book
+    // start book content
     bookContentManage = 'bookContentManage',
     bookContentSave = 'bookContentSave',
     bookContentEdit = 'bookContentEdit',
+    // end book content
+    // start person
     personManage = 'personManage',
     personSave = 'personSave',
     personEdit = 'personEdit',
+    // end person
+    // start user
     userManage = 'userManage',
     userSave = 'userSave',
     userEdit = 'userEdit',
+    // end user
+    // start comment
     commentManage = 'commentManage',
+    // end comment
+    // start order
     orderManage = 'orderManage',
     orderSave = 'orderSave',
     orderEdit = 'orderEdit',
+    // end order
+    // start group
     groupManage = 'groupManage',
     groupSave = 'groupSave',
     groupEdit = 'groupEdit',
+    // end group
+    // start transaction
     transactionManage = 'transactionManage',
+    // end transaction
 }
 
 export enum CHECKTYPE {
     ALL = 'ALL',
     ONE_OF_ALL = 'ONE_OF_ALL',
+    ONE_OF_ITEM_ALL = 'ONE_OF_ITEM_ALL',
+}
+
+export enum  CONDITION_COMBINE {
+    DOSE_NOT_HAVE = 'DOSE_NOT_HAVE',
+    OR = 'OR',
+    AND = 'AND',
+}
+
+export enum  EXTERA_FUN_NAME {
+    orderCheckoutAccess = 'orderCheckoutAccess',
 }
